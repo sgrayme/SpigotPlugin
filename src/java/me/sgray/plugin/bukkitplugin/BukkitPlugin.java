64bb471d447ab38498ddbcc9f561b58c190fa1ae
Shaila Gray <shailaweb@yahoo.com>
2014-09-12 22:01:24 -0500

    Add reminder for commands

--- src/java/me/sgray/plugin/bukkitplugin/BukkitPlugin.java
+++ src/java/me/sgray/plugin/bukkitplugin/BukkitPlugin.java
@@ -6,11 +6,14 @@ public class BukkitPlugin extends JavaPlugin {
     @Override
     public void onDisable() {
         // Don't log disabling, Bukkit does that for you automatically!
-        //
     }
 
     @Override
     public void onEnable() {
-       // Don't log enabling, Bukkit does that for you automatically!
+        // Don't log enabling, Bukkit does that for you automatically!
+
+        // Add commands to plugin.yml if using one of following snippets.
+        // getCommand.setExecutor(this);
+        // getCommand.setExecutor(new MyPluginCommand(this));
     }
 }
